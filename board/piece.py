aa94963e74c39a11d6f2588261c5bbc1640aaece
zedaes <147769903+zedaes@users.noreply.github.com>
2024-08-17 17:47:50 -0400

    initial commit

---
+++ board/piece.py
@@ -0,0 +1,21 @@
+from position import Position
+
+class Piece:
+    def __init__(self, color = 0, position = Position(0, 0)):
+        self.color = color
+        self.position = position
+        self.x = self.position.row
+        self.y = self.position.column
+    
+    def move(self, newPosition = Position(0, 0)):
+        if self.isValidMove(newPosition):
+            self.position = newPosition
+        else:
+            raise ValueError("Invalid move for this piece.")
+    
+    def isValidMove(self, newPosition = Position(0, 0), board = None):
+        raise NotImplementedError("This method should be overridden by subclasses.")
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.color}, {self.position})"
+
---
+++ board/position.py
@@ -0,0 +1,29 @@
+class Position:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+    
+    @classmethod
+    def fromAlgebraic(cls, algebraicNotation):
+        column = ord(algebraicNotation[0]) - ord('a')
+        row = int(algebraicNotation[1]) - 1
+        return cls(row, column)
+    
+    def toAlgebraic(self):
+        column = chr(self.col + ord('a'))
+        row = str(self.row + 1)
+
+        return column + row
+    
+    def __eq__(self, other):
+        return self.row == other.row and self.column == other.column
+    
+    def __str__(self):
+        return self.toAlgebraic()
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def isValid(self):
+        return 0 <= self.row < 8 and 0 <= self.column < 8
+
