ec76d4b81af80a7abea059071e898e5b6a328973
zedaes <147769903+zedaes@users.noreply.github.com>
2024-08-18 15:22:19 -0400

    idk

--- board/board.py
+++ board/board.py
@@ -12,7 +12,8 @@ class Board:
     def __init__(self):
         self.board = [[None for _ in range(8)] for _ in range(8)]
     
-    def placePiece(self, piece, position):
+    def placePiece(self, piece):
+        position = piece.position
         if self.isWithinBounds(position) and self.board[position.row][position.column] is None:
             self.board[position.row][position.column] = piece
             piece.position = position
@@ -46,41 +47,14 @@ class Board:
             Queen: "Q",
             King: "K"
         }
-        boardStr = ""
+        boardString = ""
         for row in self.board:
             for cell in row:
                 if cell is None:
-                    boardStr += ". "
+                    boardString += ". "
                 else:
-                    boardStr += piece_symbols.get(type(cell), "X") + " "
-            boardStr += "\n"
-        return boardStr
-    
-if __name__ == "__main__":
-    board = Board()
-    pawnWhite = Pawn(color=0, position=Position(1, 0))
-    pawnBlack = Pawn(color=1, position=Position(6, 0))
-    rookWhite = Rook(color=0, position=Position(0, 0))
-    knightWhite = Knight(color=0, position=Position(0, 1))
-    bishopWhite = Bishop(color=0, position=Position(0, 2))
-    queenWhite = Queen(color=0, position=Position(0, 3))
-    kingWhite = King(color=0, position=Position(0, 4))
-    
-    board.placePiece(pawnWhite, Position(1, 0))
-    board.placePiece(pawnBlack, Position(6, 0))
-    board.placePiece(rookWhite, Position(0, 0))
-    board.placePiece(knightWhite, Position(0, 1))
-    board.placePiece(bishopWhite, Position(0, 2))
-    board.placePiece(queenWhite, Position(0, 3))
-    board.placePiece(kingWhite, Position(0, 4))
-    
-    print("Initial Board:")
-    print(board)
-    
-    board.movePiece(pawnWhite, Position(3, 0))  # White pawn moves two steps forward
-    print("After White Pawn Move:")
-    print(board)
-    
-    board.movePiece(pawnBlack, Position(5, 0))  # Black pawn moves one step forward
-    print("After Black Pawn Move:")
-    print(board)
+                    boardString += piece_symbols.get(type(cell), "X") + " "
+            boardString += "\n"
+            
+        return boardString
+   
