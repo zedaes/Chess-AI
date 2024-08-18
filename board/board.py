9a95d8e7f12128e3116a90667fcf3af9d6b097d4
zedaes <147769903+zedaes@users.noreply.github.com>
2024-08-17 20:17:28 -0400

    idk

--- board/board.py
+++ board/board.py
@@ -34,6 +34,9 @@ class Board:
         else:
             raise ValueError("Invalid move")
         
+    def isWithinBounds(self, position):
+        return 0 <= position.row < 8 and 0 <= position.column < 8
+        
     def __str__(self):
         piece_symbols = {
             Pawn: "P",
@@ -52,3 +55,32 @@ class Board:
                     boardStr += piece_symbols.get(type(cell), "X") + " "
             boardStr += "\n"
         return boardStr
+    
+if __name__ == "__main__":
+    board = Board()
+    pawnWhite = Pawn(color=0, position=Position(1, 0))
+    pawnBlack = Pawn(color=1, position=Position(6, 0))
+    rookWhite = Rook(color=0, position=Position(0, 0))
+    knightWhite = Knight(color=0, position=Position(0, 1))
+    bishopWhite = Bishop(color=0, position=Position(0, 2))
+    queenWhite = Queen(color=0, position=Position(0, 3))
+    kingWhite = King(color=0, position=Position(0, 4))
+    
+    board.placePiece(pawnWhite, Position(1, 0))
+    board.placePiece(pawnBlack, Position(6, 0))
+    board.placePiece(rookWhite, Position(0, 0))
+    board.placePiece(knightWhite, Position(0, 1))
+    board.placePiece(bishopWhite, Position(0, 2))
+    board.placePiece(queenWhite, Position(0, 3))
+    board.placePiece(kingWhite, Position(0, 4))
+    
+    print("Initial Board:")
+    print(board)
+    
+    board.movePiece(pawnWhite, Position(3, 0))  # White pawn moves two steps forward
+    print("After White Pawn Move:")
+    print(board)
+    
+    board.movePiece(pawnBlack, Position(5, 0))  # Black pawn moves one step forward
+    print("After Black Pawn Move:")
+    print(board)
