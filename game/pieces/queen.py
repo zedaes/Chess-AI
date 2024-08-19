7be2cae080435f614fc15754144c398ce3cead86
zedaes <147769903+zedaes@users.noreply.github.com>
2024-08-18 22:15:23 -0400

    idk

--- game/pieces/queen.py
+++ game/pieces/queen.py
@@ -32,21 +32,28 @@ class Queen(Piece):
                     return False
             return True
         
-        if abs(deltaX) == 0 and abs(deltaY) > 0:
+        if deltaX == 0 and abs(deltaY) > 0:
+            stepY = deltaY // abs(deltaY)
             for i in range(1, abs(deltaY)):
-                x = self.position.row
                 y = self.position.column + stepY * i
-                if board.board[x][y] is not None:
+                if board.board[self.position.row][y] is not None:
                     return False
             return True
             
-        if abs(deltaY) == 0 and abs(deltaX) > 0 
+        if deltaY == 0 and abs(deltaX) > 0:
+            stepX = deltaX // abs(deltaX)
+            for i in range(1, abs(deltaX)):
+                x = self.position.row + stepX * i
+                if board.board[x][self.position.column] is not None:
+                    return False
+            return True
+        
         return False
               
     def possibleMoves(self, board=None):
         possibleMoves = []
-        for i in range(0, 8):
-            for j in range(0, 8):
+        for i in range(8):
+            for j in range(8):
                 newPosition = Position(i, j)
                 if newPosition != self.position:
                     if self.isValidMove(newPosition, board):
