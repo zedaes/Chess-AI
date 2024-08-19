e62622b1efee5dd152122718d1b4472cfe6b5edf
zedaes <147769903+zedaes@users.noreply.github.com>
2024-08-18 22:12:28 -0400

    checkpoint

--- game/pieces/queen.py
+++ game/pieces/queen.py
@@ -11,3 +11,45 @@ class Queen(Piece):
             self.image = 'img/whiteQueen.png'
         else:
             self.image = 'img/blackQueen.png'
+            
+    def isValidMove(self, newPosition=Position(0, 0), board=None):
+        newPositionPiece = board.board[newPosition.row][newPosition.column]
+        
+        if newPositionPiece is not None and newPositionPiece.color == self.color:
+            return False
+        
+        deltaX = newPosition.row - self.position.row
+        deltaY = newPosition.column - self.position.column
+        
+        if abs(deltaX) == abs(deltaY) and abs(deltaX) > 0:
+            stepX = deltaX // abs(deltaX)
+            stepY = deltaY // abs(deltaY)
+            
+            for i in range(1, abs(deltaX)):
+                x = self.position.row + stepX * i
+                y = self.position.column + stepY * i
+                if board.board[x][y] is not None:
+                    return False
+            return True
+        
+        if abs(deltaX) == 0 and abs(deltaY) > 0:
+            for i in range(1, abs(deltaY)):
+                x = self.position.row
+                y = self.position.column + stepY * i
+                if board.board[x][y] is not None:
+                    return False
+            return True
+            
+        if abs(deltaY) == 0 and abs(deltaX) > 0 
+        return False
+              
+    def possibleMoves(self, board=None):
+        possibleMoves = []
+        for i in range(0, 8):
+            for j in range(0, 8):
+                newPosition = Position(i, j)
+                if newPosition != self.position:
+                    if self.isValidMove(newPosition, board):
+                        possibleMoves.append(newPosition)
+                        
+        return possibleMoves
